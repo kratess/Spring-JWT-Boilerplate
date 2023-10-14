0f539b51e6375cbb4d72a37d1b4c92ab3a1b6af6
kratess <parvudoruionut@yahoo.ro>
2023-10-14 23:30:05 +0200

    refactor general exception handler

---
+++ src/main/java/dev/kratess/boilerplate/config/handler/GeneralExceptionHandler.java
@@ -0,0 +1,19 @@
+package dev.kratess.boilerplate.config.handler;
+
+import dev.kratess.boilerplate.dao.ResponseHandler;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> createResponseEntity(Object body, HttpHeaders headers, HttpStatusCode statusCode,
+                                                          WebRequest request) {
+        HttpStatus httpStatus = HttpStatus.resolve(statusCode.value());
+
+        if (httpStatus == null) return ResponseHandler.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseHandler.generateErrorResponse(httpStatus, ((ProblemDetail) body).getDetail());
+    }
+}
